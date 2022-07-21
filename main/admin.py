ae1fa7d19d644c15d4de0278c60a59905d40f7cb
gabriel ufot <gabrielufot23@gmail.com>
2022-07-21 09:48:43 +0100

    customuseradmin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 # from .models import Hospital, Staff, BaseUser
 from .models import Hospital, Staff
+from .models import BaseUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
+
+
+class CustomUserAdmin(UserAdmin):
+    """
+    class for custom user admin to user look in admin
+    """
+    model = BaseUser
+    add_form = CustomUserCreationForm
 
 # Register your models here.
 admin.site.register(Hospital)
 admin.site.register(Staff)
-# admin.site.register(BaseUser)
+admin.site.register(BaseUser, CustomUserAdmin)
--- main/forms.py
+++ main/forms.py
@@ -1,2 +1,15 @@
-# from django import forms
-# from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import BaseUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """
+    class for custom creation form
+    """
+    class Meta:
+        """
+        meta class for custom creation forms
+        """
+        model = BaseUser
+        fields = "__all__"
