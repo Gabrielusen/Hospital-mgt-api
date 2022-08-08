f322a942d243c6b7d44b81aedabc58e6f79c63ec
gabriel ufot <gabrielufot23@gmail.com>
2022-08-08 15:46:00 +0100

    environments variable

--- hospital_mgt/settings.py
+++ hospital_mgt/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o+0ol!np#%pbfxmij7c!3chqe)dw(yp*-f2^h7u+l4(+_2gk**'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
